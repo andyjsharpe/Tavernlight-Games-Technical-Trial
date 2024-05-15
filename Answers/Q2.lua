6dd1af10f2ae80feb641722eb0ee2dcef4097dfc
Andy Sharpe <48738245+andyjsharpe@users.noreply.github.com>
2024-05-15 17:32:04 -0400

    Completed Question 2

--- Answers/Q2.lua
+++ Answers/Q2.lua
@@ -1,6 +1,26 @@
+-- This function prints names of all guilds that have less than memberCount max members
+function printSmallGuildNames(memberCount)
+    -- SQL query for getting the names of the guilds with small enough memberCounts
+    local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;"
+    local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount))
 
+    --[[
+        This follows the format found in the other files in theforgottenserver where first the result is
+        checked to ensure it returned properly, then its values are looped over in a repeat statement.
+        This needs to be done as the storeQuery is likely to return more than one name.
+    ]]--
+    if resultId ~= false then
+		repeat
+            local guildName = result.getString("name")
+			print(guildName)
+		until not result.next(resultId)
+		result.free(resultId)
+	end
+end
 
 --[[
+    The original code:
+
     Q2 - Fix or improve the implementation of the below method
 
     function printSmallGuildNames(memberCount)
